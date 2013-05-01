53911ea03be1b654513a1f2c7026b7f03fa54fd7
Dominik <sofengii@gmail.com>
2013-05-01 15:19:21 +0200

    initial Commit

---
+++ AwarenessSystem/src/main/java/Team5/AwarenessSystem/App.java
@@ -0,0 +1,13 @@
+package Team5.AwarenessSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
