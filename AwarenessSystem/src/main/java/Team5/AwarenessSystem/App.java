a70d9e92243dc20dfce7061b869e3fb587a87211
Dominik <sofengii@gmail.com>
2013-05-10 17:19:54 +0200

    test

--- AwarenessSystem/src/main/java/Team5/AwarenessSystem/App.java
+++ AwarenessSystem/src/main/java/Team5/AwarenessSystem/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("I say Hello, too!");
-        System.out.println("");
+        
     }
 }
